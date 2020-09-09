762f4be84e1bf9ff5829c1a79e837ce13cb9715a
Gesse Risco <1320818@utp.edu.pe>
2020-09-09 00:35:17 -0500

    primer commit de estructura de carpetas y proyecto basico en Flask

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_restx import Api
+
+app = Flask(__name__)
+
+api = Api(
+        app,
+        title='Proyecto Final Backend',
+        version='1.0',
+        description='aplicacion CRUD',
+    )
